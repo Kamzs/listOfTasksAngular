8c26c8b7e368d1487601f43cafe8265bf6fd9c4b
Kamzs <kamilambroziak@interia.pl>
2019-12-26 21:09:04 +0100

    lista zadan przed podzialem na moduly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lista-zadan';
+
+  task: string;
+  listOfTasks: Array<string> = [];
+  listOfTasksDone: Array<string> = [];
+
+  constructor() {}
+
+  addToList() {
+    this.listOfTasks.push(this.task);
+    this.task = '';
+  }
+
+  remove(element: string){
+    this.listOfTasks = this.listOfTasks.filter( elListy => elListy !== element);
+  }
+
+  done(element: string){
+    this.listOfTasksDone.push(element);
+    this.listOfTasks = this.listOfTasks.filter( elListy => elListy !== element);
+  }
+
 }
